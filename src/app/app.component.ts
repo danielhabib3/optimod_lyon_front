e26ae9fe0e4e47317cccd18e208c0bb26a667f1c
Adrian <adriansaleh061@gmail.com>
2024-12-09 09:54:17 +0100

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MapComponent, HeaderComponent], 
+  imports: [MapComponent, HeaderComponent, FooterComponent], 
   templateUrl: './app.component.html',
 })
 export class AppComponent {
